62a3fc3cd4c094a041c4e2d41f310bd20c9ef21e
dexvale <dextervale87@gmail.com>
2025-02-21 21:48:45 +0800

    message

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        ArrayStack ar = new ArrayStack(6);
+        ar.push(1);
+        ar.push(2);
+        System.out.println(ar.pop());
+        System.out.println(ar.peek());
         
     }
 }
--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -2,27 +2,44 @@ public class ArrayStack {
     int[] stack;
     int top;
     int capacity;
-
+                    //capacity = 6
     public ArrayStack(int capacity) {
+                        //6
         stack = new int[capacity];
         top = -1;
         this.capacity = capacity;
     }
-
-void push(int value){
-if(top == capacity - 1){
-throw new StackOverflowError("Stack is full");
-}
-stack[++top] = value;
-}
-
-int pop(){
-    if(isEmpty()){
-
+    //Adds value on top and increases top
+                //1
+    void push(int value) {
+            //-1     c=6
+        if (top == capacity - 1) {
+            throw new StackOverflowError("Stack is full");
+        }
+        //-1+1 = 0     v = 1
+        stack[++top] = value;
     }
+
+
+    boolean isEmpty() {
+        return top == -1;
+    }
+//Removes the top value and decreases top.
+    int pop(){
+        if(isEmpty()){
+            throw new IllegalStateException ("Stack is empty");
+        }
+        return stack[top--];
+    }
+//Returns the top value without removing it.
+    int peek(){
+        if(isEmpty()){
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack[top];
+    }
+
+
 }
 
-boolean isEmpty(){
-    return top == -1;
-}
-}
+
