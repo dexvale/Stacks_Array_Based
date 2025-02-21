cf32519ecea96acbb8e5412d621120055fa973b0
dexvale <dextervale87@gmail.com>
2025-02-21 23:15:38 +0800

    commit

--- src/App.java
+++ src/App.java
@@ -5,6 +5,15 @@ public class App {
         ar.push(2);
         System.out.println(ar.pop());
         System.out.println(ar.peek());
-        
+
+        //----------------------------------------
+        //----------------------------------------
+System.out.println();
+        LinkedList_Stack ls = new LinkedList_Stack();
+        ls.push(1);
+        ls.push(2);
+        System.out.println(ls.pop());
+        System.out.println(ar.peek());
+
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,48 @@
+public class Node {
+    int data;
+    Node next;
+                // 1
+    public Node(int d){
+        this.data = d;
+        next = null;
+    }
+
+}
+
+class LinkedList_Stack{
+    Node top;
+
+    //Adds value on top and increases top
+            //  v = 1
+    void push(int value){
+                                //1
+        Node newNode = new Node(value);
+        newNode.next = top;
+        top = newNode;
+
+    }
+    boolean isEmpty(){
+        return top == null;
+    }
+    //Removes the top value and decreases top.
+    int pop(){
+        if(isEmpty()){
+            throw new IllegalStateException("Stack is emptry");
+        }
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    int peek(){
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.data;
+    }
+
+
+
+
+
+}
